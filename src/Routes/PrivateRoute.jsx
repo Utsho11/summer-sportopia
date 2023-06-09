6b58a778fa88bfb51a3d8878669ca02603cb20bf
Utsho11 <utshoroy521@gmail.com>
2023-06-09 16:31:53 +0600

    PrivateRoute added

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate, useLocation } from "react-router-dom";
+import UseAuth from "../hooks/UseAuth";
+
+
+const PrivateRoute = ({children}) => {
+    const {user,loading} = UseAuth();
+    const location = useLocation();
+    
+    if(loading)
+    {
+        return <div className='text-center'><progress className="mt-48 progress w-56 h-20"></progress></div>
+    }
+    if(user)
+    {
+        return children;
+    }
+    return <Navigate to="/login" state={{from: location}} replace></Navigate>
+};
+
+export default PrivateRoute;
